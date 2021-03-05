5e2c6542042ba7c7e83abd1cae4463c6e1721814
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-03-05 18:40:50 -0300

    Schema name
    Adjustment to replace the constraint reference on tables without the schema name.

--- SQL Server - Criação de constraints.ps1
+++ SQL Server - Criação de constraints.ps1
@@ -7,25 +7,35 @@
 
 Clear-Host;
 
-Write-Host "Ajuste na criação das constraints FK"
+# Write-Host "Ajuste na criação das constraints FK"
 
-$file = 'C:\Users\vladimir\Documents\Criação de constraints.sql'
-
+$file = 'C:\Users\vladimirvargas\Documents\ANEEL\SRD DM01 - Perdas técnicas - Criação de constraints.sql'
 $str = @()
 $str += Get-Content $file
+
+$schema_name = "sch_name"
 
 $i = 0
 foreach ($s in $str) {
     $tmp = $s
-    if($s -match 'create table '){
+
+    if($s -match 'create table ') {
         $s = "if object_id('" + ($s -replace 'create table ' -replace ' \(').Split('.')[0] + "." + ($s -replace 'create table ' -replace ' \(').Split('.')[1] + "') is null"
         $s += "`r`n" + $tmp
     }
-    # $tmp = $null
+
     $tmp = $str[$i - 0]
     if($s -match 'add constraint') {
         $s = ("if object_id('" + ($str[$i - 1] -replace 'alter table ').Substring(0, ($str[$i - 1] -replace 'alter table ').IndexOf('.')) + "." + $s.Substring($s.IndexOf('FK'), $s.IndexOf('foreign') - $s.IndexOf('FK')-1) + "') is null") #Nome da FK
         $str[$i - 2] = $s
+    }
+
+    if ($s.Contains("references Dim")) {
+        $s = $s.Replace("references Dim", "references $schema_name.Dim")
+    } elseif ($s.Contains("references Fat")) {
+        $s = $s.Replace("references Fat", "references $schema_name.Fat")
+    } elseif ($s.Contains("references Agr")) {
+        $s = $s.Replace("references Agr", "references $schema_name.Agr")
     }
 
     $i++
