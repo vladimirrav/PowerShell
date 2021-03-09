199e76a198ca750c3be36d99ae6f1d0457a9f349
vladimirrav <vladimirrav@github.com>
2021-03-09 17:13:41 -0300

    Github repositores

---
+++ Github.ps1
@@ -0,0 +1,17 @@
+﻿Write-Host "Github"
+
+$documents = [Environment]::GetFolderPath("MyDocuments")
+
+Set-Location -Path $documents
+git init
+
+git clone https://vladimirrav@github.com/vladimirrav/PowerShell.git
+git clone https://vladimirrav@github.com/vladimirrav/T-SQL.git
+
+Set-Location -Path $documents\PowerShell
+Get-Location
+git pull
+
+Set-Location -Path $documents\T-SQL
+Get-Location
+git pull
