2efdf27d5696d88f3fc7b7957231beb96b66b5b9
vladimirrav <vladimirrav@github.com>
2021-03-09 17:23:31 -0300

    Github repositores

--- Github.ps1
+++ Github.ps1
@@ -5,12 +5,22 @@ $documents = [Environment]::GetFolderPath("MyDocuments")
 Set-Location -Path $documents
 git init
 
+# Config ------------------------------------------------
+    git config --global user.email "vladimirrav@github.com"
+    git config --global user.name "vladimirrav"
+# -------------------------------------------------------
+
 git clone https://vladimirrav@github.com/vladimirrav/PowerShell.git
 git clone https://vladimirrav@github.com/vladimirrav/T-SQL.git
 
 Set-Location -Path $documents\PowerShell
 Get-Location
+
 git pull
+git status
+git add .
+git commit -m "Github repositores"
+git push
 
 Set-Location -Path $documents\T-SQL
 Get-Location
