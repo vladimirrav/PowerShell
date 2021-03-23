9281a8e858b712aa55b2015df8f424a06713051f
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-03-23 11:46:22 -0300

    Script imporovement;
    Line endings with semi-colon.

--- SQL Server - Criação de constraints.ps1
+++ SQL Server - Criação de constraints.ps1
@@ -23,28 +23,28 @@ $schema_name = "sch_name";
 $i = 0;
 foreach ($s in $str)
 {
-    $tmp = $s
+    $tmp = $s;
 
     if($s -match 'create table ')
     {
-        $s = "if object_id('" + ($s -replace 'create table ' -replace ' \(').Split('.')[0] + "." + ($s -replace 'create table ' -replace ' \(').Split('.')[1] + "') is null"
-        $s += "`r`n" + $tmp
+        $s = "if object_id('" + ($s -replace 'create table ' -replace ' \(').Split('.')[0] + "." + ($s -replace 'create table ' -replace ' \(').Split('.')[1] + "') is null";
+        $s += "`r`n" + $tmp;
     };
 
     $tmp = $str[$i - 0];
     if($s -match 'add constraint')
     {
-        $s = ("if object_id('" + ($str[$i - 1] -replace 'alter table ').Substring(0, ($str[$i - 1] -replace 'alter table ').IndexOf('.')) + "." + $s.Substring($s.IndexOf('FK'), $s.IndexOf('foreign') - $s.IndexOf('FK')-1) + "') is null") #Nome da FK
-        $str[$i - 2] = $s
+        $s = ("if object_id('" + ($str[$i - 1] -replace 'alter table ').Substring(0, ($str[$i - 1] -replace 'alter table ').IndexOf('.')) + "." + $s.Substring($s.IndexOf('FK'), $s.IndexOf('foreign') - $s.IndexOf('FK')-1) + "') is null"); #Nome da FK
+        $str[$i - 2] = $s;
     };
 
     if ($s.Contains("references Dim"))
     {
-        $s = $s.Replace("references Dim", "references $schema_name.Dim")
+        $s = $s.Replace("references Dim", "references $schema_name.Dim");
     }
     elseif ($s.Contains("references Fat"))
     {
-        $s = $s.Replace("references Fat", "references $schema_name.Fat")
+        $s = $s.Replace("references Fat", "references $schema_name.Fat");
     }
     elseif ($s.Contains("references Agr"))
     {
