b5d3f1a0bf8e51dd3e5c4a9ef831ba55085106f2
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-04-07 17:23:22 -0300

    Sum elements of array
    Getting the sum of all elements in an array.

---
+++ Sum elements of array.ps1
@@ -0,0 +1,13 @@
+﻿# Array of values
+$vr = @()
+$vr += 6261.69
+$vr += -22.5
+$vr += -174.1
+$vr += -446.5
+$vr += -112.31
+$vr += -254.88
+
+# Sum all elements
+$s = 0
+$vr | ForEach-Object {$s += $_}
+Write-Host "Total:", ('{0:N}' -f $s)
