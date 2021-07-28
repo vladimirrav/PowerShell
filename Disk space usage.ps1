0b4b14c29a9a20c590cc102469501df11d83543d
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-07-28 20:17:15 -0300

    Information about multiple machines and output to text file
    Added a loop to find information on disk usage for multiple computers and output the information collected to a text file.

--- Disk space usage.ps1
+++ Disk space usage.ps1
@@ -1,38 +1,65 @@
 ﻿Clear-Host;
 
-$ComputerName = $env:COMPUTERNAME;
+$ComputerList = @();
+$ComputerList += $env:COMPUTERNAME;
 
-Write-Host "Date:"(Get-Date).ToString("dd-MM-yyyy HH:mm:ss");
-Write-Host "ComputerName: $ComputerName";
+$bar_scale = 0.3;
+$txt_out = $true;
+$txt_file = [Environment]::GetFolderPath("MyDocuments") + "\disk_space_usage.txt";
 
-$disk = Get-WmiObject Win32_LogicalDisk -ComputerName $ComputerName | #-Filter "DeviceID='C:'" |
-Select-Object DeviceID, Size, FreeSpace, VolumeName;
+Remove-Item -Path $txt_file -ErrorAction SilentlyContinue;
 
-foreach ($d in $disk)
+foreach ($ComputerName in $ComputerList)
 {
-    try
-    {    
-        Write-Host "Drive"$d.DeviceID $d.VolumeName;
-        Write-Host "`tTotal Space: " -NoNewline;
-        Write-Host ([math]::Round($d.Size / 1GB, 2).toString())"GB" -ForegroundColor Cyan;
 
-        Write-Host ([char]10006) -ForegroundColor Red -NoNewline;
-        Write-Host "`tUsed space:  " -NoNewline;
-        Write-Host ([math]::Round(($d.Size - $d.FreeSpace) / 1GB, 2).toString())"GB" -ForegroundColor Red -NoNewline;
-        Write-Host " "([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100)"%" -ForegroundColor Red;
+    Write-Host "Date".PadRight(12, ' ')(Get-Date).ToString("dd-MM-yyyy HH:mm:ss");
+    Write-Host "ComputerName".PadRight(12, ' ')$ComputerName;
+    Write-Host ("-" * 35);
 
-        Write-Host ([char]10004) -ForegroundColor Green -NoNewline;
-        Write-Host "`tFree space:  " -NoNewline;
-        Write-Host ([math]::Round($d.FreeSpace / 1GB, 2).toString())"GB" -ForegroundColor Green -NoNewline;
-        Write-Host " "([Math]::Round($d.FreeSpace / $d.Size, 2) * 100)"%" -ForegroundColor Green;
+    $disk = Get-WmiObject Win32_LogicalDisk -ComputerName $ComputerName | #-Filter "DeviceID='C:'" |
+    Select-Object DeviceID, Size, FreeSpace, VolumeName;
 
-        Write-Host "`t" -NoNewline
-        Write-Host (([char]9611).toString() * (([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100)* 0.3)) -ForegroundColor Red -NoNewline;
-        Write-Host (([char]9611).toString() * (([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) * 0.3)) -ForegroundColor Green 
-    }
-    catch
+    Add-Content -Path $txt_file -Value ("Date`t" + (Get-Date).ToString("dd-MM-yyyy HH:mm:ss"));
+    Add-Content -Path $txt_file -Value "ComputerName`t$ComputerName";
+    Add-Content -Path $txt_file -Value ("." * 35);
+
+    foreach ($d in $disk)
     {
+        try
+        {    
+            Write-Host "Drive"$d.DeviceID $d.VolumeName;
+            Write-Host "`tSize".PadRight(14, ' ') -NoNewline;
+            Write-Host ([math]::Round($d.Size / 1GB, 2).toString('0.00')).PadLeft(7, ' ')"GB" -ForegroundColor Cyan;
 
+            Write-Host ([char]10006) -ForegroundColor Red -NoNewline;
+            Write-Host "`tUsed space".PadRight(14, ' ') -NoNewline;
+            Write-Host ([math]::Round(($d.Size - $d.FreeSpace) / 1GB, 2).toString('0.00')).PadLeft(7, ' ')"GB" -ForegroundColor Red -NoNewline;
+            Write-Host " "([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100)"%" -ForegroundColor Red;
+
+            Write-Host ([char]10004) -ForegroundColor Green -NoNewline;
+            Write-Host "`tFree space".PadRight(14, ' ') -NoNewline;
+            Write-Host ([math]::Round($d.FreeSpace / 1GB, 2).toString('0.00')).PadLeft(7, ' ')"GB" -ForegroundColor Green -NoNewline;
+            Write-Host " "([Math]::Round($d.FreeSpace / $d.Size, 2) * 100)"%" -ForegroundColor Green;
+
+            Write-Host "`t" -NoNewline
+            Write-Host (([char]9611).toString() * (([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) * $bar_scale)) -ForegroundColor Red -NoNewline;
+            Write-Host (([char]9611).toString() * (([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) * $bar_scale)) -ForegroundColor Green;
+
+            ### Write to file
+
+            Add-Content -Path $txt_file -Value ("Drive " + $d.DeviceID + "`t" + $d.VolumeName);
+            Add-Content -Path $txt_file -Value ((" `tSize`t".PadRight(11, ' ') + ([math]::Round($d.Size / 1GB, 2).toString('0.00')) + " GB"));
+            Add-Content -Path $txt_file -Value ("-" + "`tUsed space`t" + ([math]::Round(($d.Size - $d.FreeSpace) / 1GB, 2).toString('0.00'))+ " GB" + "`t" + ([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) + "%");
+            Add-Content -Path $txt_file -Value ("+" + "`tFree space`t" + ([math]::Round($d.FreeSpace / 1GB, 2).toString('0.00')) + " GB" + "`t" + ([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) + "%");
+            Add-Content -Path $txt_file -Value (" `t" + ("O" * (([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) * $bar_scale)) + ("." * (([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) * $bar_scale)));
+            Add-Content -Path $txt_file -Value ("" * 35);
+        }
+        catch
+        {
+            Write-Host;
+        };
+        Write-Host;
     };
-    Write-Host;
+    Add-Content -Path $txt_file -Value (("-" * 35) + "`r`n");
+    Write-Host ("-" * 35);
 };
