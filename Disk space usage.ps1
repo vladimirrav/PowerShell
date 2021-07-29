f4f4c4bc09d52d40ed8c165ba7ca574e0d6418bf
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-07-29 20:12:50 -0300

    Character encoding
    Character encoding for better visualization on the text file that is written as output. Better visualization in PowerShell ISE, some characters are not displayed in PowerShell console.

--- Disk space usage.ps1
+++ Disk space usage.ps1
@@ -1,4 +1,14 @@
-﻿Clear-Host;
+﻿<#
+    .SYNOPSIS
+        Space in disk
+
+    .DESCRIPTION
+        Space used on all hard drives of the computers listed in the array variable $ComputerList.
+
+    .OUTPUTS
+        Illustration of the space taken and the free space on each drive.
+#>
+Clear-Host;
 
 $ComputerList = @();
 $ComputerList += $env:COMPUTERNAME;
@@ -12,7 +22,7 @@ foreach ($ComputerName in $ComputerList)
 {
     Write-Host "Date".PadRight(12, ' ')(Get-Date).ToString("dd-MM-yyyy HH:mm:ss");
     Write-Host "ComputerName".PadRight(12, ' ')$ComputerName;
-    Write-Host ("-" * 35);
+    Write-Host ("-" * 38);
 
     <#  # Older, sometimes only the Get-WmiObject works on the machine
         $disk = Get-WmiObject Win32_LogicalDisk -ComputerName $ComputerName -Filter "DriveType=3" |
@@ -25,7 +35,7 @@ foreach ($ComputerName in $ComputerList)
 
     Add-Content -Path $txt_file -Value ("Date`t" + (Get-Date).ToString("dd-MM-yyyy HH:mm:ss"));
     Add-Content -Path $txt_file -Value "ComputerName`t$ComputerName";
-    Add-Content -Path $txt_file -Value ("." * 35);
+    Add-Content -Path $txt_file -Value ("." * 38);
 
     foreach ($d in $disk)
     {
@@ -46,17 +56,17 @@ foreach ($ComputerName in $ComputerList)
             Write-Host " "([Math]::Round($d.FreeSpace / $d.Size, 2) * 100)"%" -ForegroundColor Green;
 
             Write-Host "`t" -NoNewline
-            Write-Host (([char]9611).toString() * (([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) * $bar_scale)) -ForegroundColor Red -NoNewline;
-            Write-Host (([char]9611).toString() * (([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) * $bar_scale)) -ForegroundColor Green;
+            Write-Host (([char]9608).ToString() * (([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) * $bar_scale)) -ForegroundColor Red -NoNewline;
+            Write-Host (([char]9608).ToString() * (([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) * $bar_scale)) -ForegroundColor Green;
 
             ### Write to file
 
             Add-Content -Path $txt_file -Value ("Drive " + $d.DeviceID + "`t" + $d.VolumeName);
-            Add-Content -Path $txt_file -Value ((" `tSize`t".PadRight(11, ' ') + ([math]::Round($d.Size / 1GB, 2).toString('0.00')) + " GB"));
-            Add-Content -Path $txt_file -Value ("-" + "`tUsed space`t" + ([math]::Round(($d.Size - $d.FreeSpace) / 1GB, 2).toString('0.00'))+ " GB" + "`t" + ([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) + "%");
-            Add-Content -Path $txt_file -Value ("+" + "`tFree space`t" + ([math]::Round($d.FreeSpace / 1GB, 2).toString('0.00')) + " GB" + "`t" + ([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) + "%");
-            Add-Content -Path $txt_file -Value (" `t" + ("#" * (([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) * $bar_scale)) + ("." * (([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) * $bar_scale)));
-            Add-Content -Path $txt_file -Value ("" * 35);
+            Add-Content -Path $txt_file -Value ((" `tSize".PadRight(12, ' ') + "`t" + ([math]::Round($d.Size / 1GB, 2).toString('0.00')) + " GB"));
+            Add-Content -Encoding UTF8 -Path $txt_file -Value (([char]10006) + "`tUsed space`t" + ([math]::Round(($d.Size - $d.FreeSpace) / 1GB, 2).toString('0.00'))+ " GB" + "`t" + ([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) + "%");
+            Add-Content -Encoding UTF8 -Path $txt_file -Value (([char]10004) + "`tFree space`t" + ([math]::Round($d.FreeSpace / 1GB, 2).toString('0.00')) + " GB" + "`t" + ([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) + "%");
+            Add-Content -Encoding UTF8 -Path $txt_file -Value (" `t" + (([char]9608).ToString() * (([Math]::Round(1 - ($d.FreeSpace / $d.Size), 2) * 100) * $bar_scale)) + (([char]9472).ToString() * (([Math]::Round($d.FreeSpace / $d.Size, 2) * 100) * $bar_scale)));
+            Add-Content -Path $txt_file -Value ("" * 38);
         }
         catch
         {
@@ -64,6 +74,6 @@ foreach ($ComputerName in $ComputerList)
         };
         Write-Host;
     };
-    Add-Content -Path $txt_file -Value (("-" * 35) + "`r`n");
-    Write-Host ("-" * 35);
+    Add-Content -Path $txt_file -Value (("-" * 38) + "`r`n");
+    Write-Host ("-" * 38);
 };
