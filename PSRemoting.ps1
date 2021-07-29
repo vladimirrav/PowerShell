9e4d82d7682cf797fac75d280cfb6651e97f3f13
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-07-29 13:17:12 -0300

    PSRemoting
    Scripts for testing PSRemoting features.

---
+++ PSRemoting.ps1
@@ -0,0 +1,20 @@
+﻿$ComputerList = @();
+$ComputerList += $env:COMPUTERNAME;
+
+foreach ($ComputerName in $ComputerList)
+{
+    Enable-PSRemoting -Force
+    (Get-CimInstance CIM_ComputerSystem -ComputerName $ComputerName).Name;
+};
+
+$ComputerName = $env:COMPUTERNAME;
+Get-CimInstance -ClassName Win32_LogicalDisk -ComputerName $ComputerName -Filter "DriveType=3" |
+Select-Object -Property DeviceID, DriveType, FreeSpace, Size, VolumeName;
+
+$ComputerName = $env:COMPUTERNAME;
+$disk = Get-WmiObject Win32_LogicalDisk -ComputerName $ComputerName -Filter "DriveType=3" |
+Select-Object DeviceID, DriveType, FreeSpace, Size, VolumeName;
+
+# Check if PSRemoting is enabled;
+Enter-PSSession -ComputerName $ComputerName;
+Exit-PSSession;
