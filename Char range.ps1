66cdf716a39f0a0c5b5d956de436745a80d2c692
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-07-28 22:35:02 -0300

    List of chars
    Displays a list of characters in a range.

---
+++ Char range.ps1
@@ -0,0 +1,16 @@
+﻿<#
+    .SYNOPSIS
+    Character list
+
+    .DESCRIPTION
+    Display characters in a range.
+
+    .OUTPUTS
+    Unicode character and its code.
+#>
+Clear-Host;
+
+8000..11500 |
+ForEach-Object {
+    Write-Host $_ ([char]$_);
+};
