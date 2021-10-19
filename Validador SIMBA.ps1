72c14f6c7fdd054281200f971f7b73b673183384
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-10-19 18:53:18 -0300

    SIMBA - Validador bancário
    Linha de comando para a execução do Validador bancário do SIMBA com configuração da quantidade de memória a ser utilizada.

---
+++ Validador SIMBA.ps1
@@ -0,0 +1,19 @@
+﻿<#
+    .SYNOPSIS
+        SIMBA - Validador bancário
+
+    .DESCRIPTION
+        Linha de comando para execução do validador bancário do SIMBA
+        com a quantidade especificada de memória.
+
+    .OUTPUTS
+        Execução da interface do validador.
+#>
+Write-Host "Executável do validador SIMBA" -ForegroundColor Yellow;
+
+$simba = ([Environment]::GetFolderPath("MyDocuments") + '\Programas SIMBA\Validador');
+Set-Location -Path $simba;
+
+Write-Host "Execução do validador" -ForegroundColor Yellow;
+
+java -Xmx8g -jar simba-validador.jar;
