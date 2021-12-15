2910648ed5a586f35ba7d0009669924ee92d3d1b
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2021-12-15 11:17:06 -0300

    Console colors
    List of available colors to write to the console.

---
+++ ConsoleColor.ps1
@@ -0,0 +1,15 @@
+﻿<#
+    .SYNOPSIS
+        Available colors for console
+    .DESCRIPTION
+        The list of available colors for writing to the console.
+    .NOTES
+        Author: Vladimir
+#>
+
+$colors = [Enum]::GetValues([System.ConsoleColor]);
+
+foreach ($color in $colors)
+{
+    Write-Host ([char]12321, (([char]9608).toString() * [int](((Get-Random -Minimum 90 -Maximum (100 + 1))/100) * 100)).PadRight(100, ' '), [char]12321) $color -ForegroundColor $color;
+};
