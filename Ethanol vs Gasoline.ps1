030eac87ed60af17dd77035df42e371c2829f2d4
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2022-01-04 19:48:28 -0300

    Ethanol vs Gasoline
    Consumption comparison between ethanol and gasoline for automobiles.

---
+++ Ethanol vs Gasoline.ps1
@@ -0,0 +1,48 @@
+﻿Clear-Host;
+$dist = 4 * 2;
+$sep = " | ";
+$kml_G = 13.6;
+$price_LG = 6.6;
+$kml_E = 11.8;
+$price_LE = 5.2;
+
+# Calc
+$km100_E = [math]::Round((100 / $kml_E), 1);
+$km100_G = [math]::Round((100 / $kml_G), 1);
+$qt_litters_G = [math]::Round(($dist / 100 * $km100_G), 1);
+$qt_litters_E = [math]::Round(($dist / 100 * $km100_E), 1);
+$value_Km_G = [math]::Round(($price_LG / $kml_G), 2);
+$value_Km_E = [math]::Round(($price_LE / $kml_E), 2);
+$value_100Km_G = [math]::Round((100 / $kml_G * $price_LG), 2);
+$value_100Km_E = [math]::Round((100 / $kml_E * $price_LE), 2);
+$value_G = [math]::Round(($price_LG * $dist / 100 * $km100_G), 2);
+$value_E = [math]::Round(($price_LE * $dist / 100 * $km100_E), 2);
+
+# Output
+Write-Host "Distance"$dist.ToString('#,##0')"Km";
+
+Write-Host "G " -NoNewline;
+Write-Host "Km/L: $kml_G"-NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "R$/Km"$value_Km_G.ToString('0.00') -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host $qt_litters_G.ToString('#,##0.0')"L" -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "R$"$value_G.ToString('#,##0.00') -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "L/100 Km: $km100_G" -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "R$/100 Km: $value_100Km_G";
+
+Write-Host "E " -NoNewline;
+Write-Host "Km/L: $kml_E"-NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "R$/Km"$value_Km_E.ToString('0.00') -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host $qt_litters_E.ToString('#,##0.0')"L" -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "R$"$value_E.ToString('#,##0.00') -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "L/100 Km: $km100_E" -NoNewline;
+Write-Host $sep -NoNewline;
+Write-Host "R$/100 Km: $value_100Km_E";
