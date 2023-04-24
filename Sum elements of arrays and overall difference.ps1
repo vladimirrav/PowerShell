4bcae8fcd70ff0ff1797a3c92c876d7eeada4c64
Vladimir <39162450+vladimirrav@users.noreply.github.com>
2023-04-24 14:31:42 -0300

    Character change for improving visual in console
    Chage in formating characters and color code if both arrays have equal value.

--- Sum elements of arrays and overall difference.ps1
+++ Sum elements of arrays and overall difference.ps1
@@ -1,15 +1,18 @@
 ﻿Clear-Host
-Write-Host ('-' * 27)
+Write-Host (([char]9644).ToString() * 30)
 
 # Array 1
 $vr_1 = @()
-$vr_1 += 7500
-$vr_1 += 300
+$vr_1 += 13000
+$vr_1 += 28.9 * 23
 
 # Array 2
 $vr_2 = @()
-$vr_2 += 8000
-$vr_2 += 1000
+$vr_2 += 13700
+$vr_2 += 200
+$vr_2 += 47 * 23
+$vr_2 += 642
+$vr_2 += [Math]::Round($vr_2[0]/12, 2) * 12/12
 
 $soma_1, $soma_2 = 0, 0
 
@@ -21,9 +24,13 @@ if ($soma_1 -gt $soma_2)
 {
     $char, $color = [char]10004, 'green'
 }
-else
+elseif ($soma_1 -lt $soma_2)
 {
     $char, $color = [char]10006, 'red'
+}
+else
+{
+    $char, $color = [char]11044, 'yellow'
 }
 
 Write-Host "$char`t" -NoNewline -ForegroundColor $color
@@ -35,9 +42,13 @@ if ($soma_2 -gt $soma_1)
 {
     $char, $color = [char]10004, 'green'
 }
-else
+elseif ($soma_2 -lt $soma_1)
 {
     $char, $color = [char]10006, 'red'
+}
+else
+{
+    $char, $color = [char]11044, 'yellow'
 }
 
 write-host "$char`t" -NoNewline -ForegroundColor $color
@@ -45,15 +56,19 @@ Write-Host "Array 2".PadRight(12,' ') -NoNewline
 
 Write-Host ("{0:N}" -f $soma_2).PadLeft(10,' ') -ForegroundColor $color
 
-Write-Host (([char]11037).ToString() * 30)
+Write-Host (([char]8226).ToString() * 30)
 
 if ($soma_2 -gt $soma_1)
 {
     $color = 'green'
 }
-else
+elseif ($soma_2 -lt $soma_1)
 {
     $color = 'red'
+}
+else
+{
+    $color = 'blue'
 }
 $char = [char]10148
 
@@ -61,4 +76,4 @@ Write-Host "$char`t" -NoNewline -ForegroundColor $color
 Write-Host "Difference".PadRight(12,' ') -NoNewline
 Write-Host ("{0:N}" -f ($soma_2 - $soma_1)).PadLeft(10,' ') -ForegroundColor $color
 
-Write-Host ('-' * 27)
+Write-Host (([char]9644).ToString() * 30)
